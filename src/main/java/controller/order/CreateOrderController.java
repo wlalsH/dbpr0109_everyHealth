8fea4e13945c2f5e155e6470b96020d1ff97cb6b
wlalsH <hwang0010@naver.com>
2022-12-02 23:41:55 +0900

    [ADD]product/view.jsp에서 바로 결제하는 부분 추가, 최종 가격 form에서 받아오는 부분 추가 [FIX]주문 생성 후 주문 확인하는 부분에 orderid 사용하는 방식으로 변경

--- src/main/java/controller/order/CreateOrderController.java
+++ src/main/java/controller/order/CreateOrderController.java
@@ -20,9 +20,12 @@ import model.Customer;
 import model.Item;
 import model.NonMemCustomer;
 import model.Order;
+import model.Product;
 import model.ShippingDetail;
+import model.service.CustomerManager;
 import model.service.OrderManager;
 import model.service.OutOfStockException;
+import model.service.ProductManager;
 
 public class CreateOrderController implements Controller {
 	private static final Logger log = LoggerFactory.getLogger(CreateOrderController.class);
@@ -32,30 +35,49 @@ public class CreateOrderController implements Controller {
 		HttpSession session = request.getSession();
 		String customerId = null;
 		Customer customer = null;
-		OrderManager manager = OrderManager.getInstance();
+		OrderManager orderManager = OrderManager.getInstance();
 		int totalPrice = 0;
+		
+		// 로그인 여부 확인
+		CustomerManager customerManager = CustomerManager.getInstance();
 		if (CustomerSessionUtils.hasLogined(session)) {
-			customerId = CustomerSessionUtils.getLoginCustomerId(session);
-//			CustomerManager에서 id를 통해 CustomerDTO 가져오기. 
+			customerId = CustomerSessionUtils.getLoginCustomerId(session); 
+			customer = customerManager.findCustomer(customerId);
 		} 
 		
+		// GET request: order form 요청
 		if (request.getMethod().equals("GET")) {	
-    		// GET request: order form 요청
     		log.debug("OrderForm Request");
     		
     		// orderForm에 회원 정보를 자동으로 기입하기 위해. 
     		if (CustomerSessionUtils.hasLogined(session)) {
-//	    		request.setAttribute("customer", c);
+	    		request.setAttribute("customer", customer);
     		}
     		
-    		request.setAttribute("totalPrice", 30000);
-    		
-    		// 장바구니에서 넘어온 item 정리해서 "items"로 넘김. 
-//    		request.setAttribute("items", );
-    		
-    		// items 정리해서 totalPrice 계산 후 넘김. 
-//    		totalPrice = manager.calcTotalPrice(items);
-//    		request.setAttibute("totalPrice", totalPrice);
+    		ArrayList<Item> items = new ArrayList<Item>();
+    		ProductManager productManager = ProductManager.getInstance();
+    		String productId = null;
+    		if ((productId = request.getParameter("productid")) != null) {		// view.jsp에서 상품 하나만 결제하는 경우
+    			Product product = productManager.findProduct(productId);
+    			
+    			Item item = new Item(productId, product.getName(), 1, product.getImage());
+    			items.add(item);
+
+    			totalPrice = product.getPrice();
+    			
+    			request.setAttribute("items", items);
+    			request.setAttribute("totalPrice", totalPrice);
+    			
+    			session.setAttribute("items", items);
+    		} else {
+    			// TODO 장바구니에서 넘어온 item 정리해서 "items"로 넘김. 
+//        		request.setAttribute("items", );
+//        		session.setAttribute("items", );
+    			
+    			// TODO items 정리해서 totalPrice 계산 후 넘김. 
+//        		totalPrice = manager.calcTotalPrice(items);
+//        		request.setAttibute("totalPrice", totalPrice);
+    		}
 		
 			return "/order/orderForm.jsp";   // orderForm으로 이동.     	
 	    }
@@ -68,10 +90,11 @@ public class CreateOrderController implements Controller {
 		String address = request.getParameter("address");					// 배송 주소
 		String crType = request.getParameter("cashReceiptType");			// 현금 영수증 (개인/사업자)
 		String crPhone = request.getParameter("cashRecepitPhone");			// 현금 영수증 번호
-		String accountHolder = request.getParameter("accountHoler");		// 입금 이름
+		String accountHolder = request.getParameter("accountHolder");		// 입금 이름
 		String bankName = request.getParameter("bankName");					// 입금 은행명
 		String shippingMessage = request.getParameter("shippingMessage");	// 배송 메세지
-		ArrayList<Item> items = (ArrayList<Item>) request.getAttribute("items");				// item
+		String finalPrice = request.getParameter("finalPrice");				// 최종 가격 
+		ArrayList<Item> items = (ArrayList<Item>) session.getAttribute("items");
 		
 		ShippingDetail sd = new ShippingDetail(address, shippingMessage);	
 		CashReceipt cr = new CashReceipt(crType, crPhone);
@@ -79,17 +102,19 @@ public class CreateOrderController implements Controller {
 		
 		Order order = null;
 		NonMemCustomer nmCustomer = null;
-		if (customerId != null) {
+		if (CustomerSessionUtils.hasLogined(session)) {
 			order = new Order(-1, new Date(), "입금 전", customerId, Integer.parseInt(usedPoint), null);
+			
 		} else {
-			order = new Order(-1, new Date(), "입금 전", null, -1, null);
+			order = new Order(-1, new Date(), "입금 전", null, 0, null);
 			nmCustomer = new NonMemCustomer(name, email, phone);
 		}
 		
+		Order newOrder;
 		try {
-			Order newOrder = manager.createOrder(order, nmCustomer, items, cr, bi, sd, totalPrice);
-			session.setAttribute("order", newOrder);
-			return "redirection:/order/orderCheck";
+			newOrder = orderManager.createOrder(order, nmCustomer, items, cr, bi, sd, Integer.parseInt(finalPrice));
+			session.removeAttribute("items");
+			return "redirect:/order/orderCheck?orderid=" + newOrder.getOrderId();
 		} catch (OutOfStockException e) {
 			request.setAttribute("exception", e);
 			return "/order/orderForm.jsp";
